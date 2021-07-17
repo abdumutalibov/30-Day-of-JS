fe7cf0fdb368e25e043f083cef451bc54d00edc9
Azimjon <dryden1309@gmail.com>
2021-07-17 06:17:48 +0500

    day #02 the end

--- #Day 02 — Data Types/script.js
+++ #Day 02 — Data Types/script.js
@@ -1,25 +1,25 @@
-// let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
-// let set=string.length
-// console.log(string.lastIndexOf('love'))       // 67
-// console.log(string.lastIndexOf('yo'))        // 63
-// console.log(string.lastIndexOf('JavaScript')) // 38
-// console.log(string.lastIndexOf('JavaScript')) // 38
-// //console.log(string.length()) // 38
-// console.log(set) // 38
-// let pattern = /love/gi
-// console.log(string.match(pattern));
+let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+let set=string.length
+console.log(string.lastIndexOf('love'))       // 67
+console.log(string.lastIndexOf('yo'))        // 63
+console.log(string.lastIndexOf('JavaScript')) // 38
+console.log(string.lastIndexOf('JavaScript')) // 38
+//console.log(string.length()) // 38
+console.log(set) // 38
+let pattern = /love/gi
+console.log(string.match(pattern));
 
-// let txt = 'In 2019, I ran 30 Days of Python. Now, in 2020 I am super exited to start this challenge'
-// let regEx = /\d+/
+let txt = 'In 2019, I ran 30 Days of Python. Now, in 2020 I am super exited to start this challenge'
+let regEx = /\d+/
 
 // d with escape character means d not a normal d instead acts a digit
 // + means one or more digit numbers,
 // if there is g after that it means global, search everywhere.
 
-// console.log(txt.match(regEx))  // ["2", "0", "1", "9", "3", "0", "2", "0", "2", "0"]
-// console.log(txt.match(/\d+/g)) // ["2019", "30", "2020"]
+console.log(txt.match(regEx))  // ["2", "0", "1", "9", "3", "0", "2", "0", "2", "0"]
+console.log(txt.match(/\d+/g)) // ["2019", "30", "2020"]
 
-//Exercise: Level 1
+// Exercise: Level 1
 
 //1.Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
 let challenge = '30 Days Of JavaScript'
@@ -85,12 +85,49 @@ console.log('5\t1\t5\t25\t125');
 12
 
 let use ='You cannot end a sentence with because because because is a conjunction'
+console.log(use.substr(30,17));
+
+// Exercises: Level 3
 
 
 
 
 
+         
+        // let d = 0;
+        // let p = 0;
+        // for ( i = 0; i < str.length; i++) {
+        //         if (str.charAt(i) == 'd') {
+        //             d++;
+        //              console.log("d ni joylashgan qatori: " + (i));
+        //         } else if (str.charAt(i) == 'p') {
+        //             p++;
+        //              console.log("p ni joylashgan qatori: " + (i));
+        //         }
+        // }
+        // console.log("text uzunligi: "+ str.length);
+        
+        // console.log("p: " , p);
+        // console.log("d: " , d);
 
+
+       
+        //    console.log('O`rta arifmetigi:',(p+d)/2 );
+        //    console.log('O`rta geometrik:',(p+d)**0.5);
+        //    console.log('Yig`indisi:' ,2+2)
+
+1
+let rstring = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log(rstring.match(/love/gi));
+
+2
+let bec = 'You cannot end a sentence with because because because is a conjunction'
+console.log(bec.match(/because/gi));
+
+3
  const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
- console.log(sentence.match(/%,$,@,#,&/gi));
+ console.log(sentence.replace(/[^a-z .,]/gi,''));
+
+4
+ console.log(5000*12+10000+15000*12);
