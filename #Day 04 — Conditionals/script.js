939ba735d84ef1b82b07a66efbf4cdb4c9152256
Farkhod Zokhidov <farafuri77@gmail.com>
2021-07-28 18:18:36 +0300

    Day 04

--- #Day 04 — Conditionals/script.js
+++ #Day 04 — Conditionals/script.js
@@ -1,1 +1,19 @@
-console.log('Hello Yopta');
+1
+let you = prompt('Enter your age:');
+// if(you>=18){
+//     console.log('You are old enough to drive.');
+// }else if(you !== 'number'){
+//     console.log('Raqam kiriting');
+// }
+// else{
+//     console.log(`You are left with ${18-you} years to drive.`);
+// }
+// you>=18 ? console.log('You are old enough to drive.'): console.log(`You are left with ${18-you} years to drive.`);
+2
+let me =prompt('Enter your age:')
+let yosh=me -2021
+if(yosh>=18){
+    console.log(`I am ${yosh} older then you `)
+}else{
+    console.log(`You are ${yosh} older then me`);
+}
