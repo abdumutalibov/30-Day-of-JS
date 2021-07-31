2407af2e3375ec106925f260c417119035b5760d
Farkhod Zokhidov <farafuri77@gmail.com>
2021-07-31 21:19:24 +0300

    Day 05

---
+++ #Day 05 — Arrays/script.js
@@ -0,0 +1,11 @@
+let arr = ['python','Node','Redux','MongoDB','JavaScript']
+console.log(arr.length);
+console.log(arr[0],arr[3],arr[4]);
+const mixedDataTypes = ['called','Declare','python','Node','Redux','MongoDB','JavaScript']
+console.log(mixedDataTypes.length);
+let itCompanies =['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' , 'Amazon']
+console.log(itCompanies);
+console.log(itCompanies.length);
+console.log(itCompanies[0],itCompanies[3],itCompanies[6]);
+console.log(itCompanies);
+console.log(itCompanies.t);
