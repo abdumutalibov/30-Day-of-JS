114699084339745cd3b1628d6bd98b69d857ae78
abdumutalibov <azimabdumutalibovazim@gmail.com>
2021-07-31 23:27:04 +0500

    JS

--- #Day 03 — Booleans, Operators, Date/script.js
+++ #Day 03 — Booleans, Operators, Date/script.js
@@ -1,82 +1,227 @@
 
-const now = new Date()
-console.log(now)
-console.log(now.getFullYear())
-console.log(now.getMonth())
-console.log(now.getDate())
-console.log(now.getHours())
-console.log(now.getMinutes())
-console.log(now.getSeconds())
-console.log(now.getTime())
+// const now = new Date()
+// console.log(now)
+// console.log(now.getFullYear())
+// console.log(now.getMonth())
+// console.log(now.getDate())
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+// console.log(now.getTime())
 
 
 
-//Exercises: Level 1
-let firstName ='Boop'
-let lastName = 'Dirt'
-let country = 'AQSH'
-let city = 'Tashken'
-let isMarried= false
-let age = 23
-let fullinfo = firstName +lastName +country +city +isMarried+age
-console.log(typeof firstName,lastName);
-console.log(typeof fullinfo  );
-console.log(typeof age);
-console.log(typeof isMarried);
-console.log('10'==10);
-console.log(parseInt('9.8') ==10);
-console.log(Math.round(9.8)===10);
+// //Exercises: Level 1
+// let firstName ='Boop'
+// let lastName = 'Dirt'
+// let country = 'AQSH'
+// let city = 'Tashken'
+// let isMarried= false
+// let age = 23
+// let fullinfo = firstName +lastName +country +city +isMarried+age
+// console.log(typeof firstName,lastName);
+// console.log(typeof fullinfo  );
+// console.log(typeof age);
+// console.log(typeof isMarried);
+// console.log('10'==10);
+// console.log(parseInt('9.8') ==10);
+// console.log(Math.round(9.8)===10);
 
-//Boolean value is either true or false.
+// //Boolean value is either true or false.
 //i
-console.log(3==3);
-let  a = 3
-b =2
-console.log(`${a} is greater than ${b}: `,a>b)
-console.log('abds'=='abds');
-//ii
-console.log(50+50==90);
-console.log('asdasd'==="adadada");
-console.log(`${a} is  greater than ${b}: `,a<b)
+// console.log(3==3);
+// let  a = 3
+// b =2
+// console.log(`${a} is greater than ${b}: `,a>b)
+// console.log('abds'=='abds');
+// //ii
+// console.log(50+50==90);
+// console.log('asdasd'==="adadada");
+// console.log(`${a} is  greater than ${b}: `,a<b)
 
-5
+// 5
 
-console.log(4 > 3);
-console.log(4 >= 3);
-console.log(4<3);
-console.log(4<=3);
-console.log(4==4);
-console.log(4===4);
-console.log(4!=4);
-console.log(4 !== 4);
-console.log(4 != '4');
-console.log(4 == '4');
-console.log(4 === '4');
-console.log('python'.length!=='jargon'.length,'');
+// console.log(4 > 3);
+// console.log(4 >= 3);
+// console.log(4<3);
+// console.log(4<=3);
+// console.log(4==4);
+// console.log(4===4);
+// console.log(4!=4);
+// console.log(4 !== 4);
+// console.log(4 != '4');
+// console.log(4 == '4');
+// console.log(4 === '4');
+// console.log('python'.length!=='jargon'.length,'');
 
-//6 Figure out the result of the following expressions first without using
-//  console.log(). After you decide the result confirm it by using console.log()
-console.log(4>3&&10<12,'i');
-console.log(4>3&&10>12);
-console.log(4<3 || 10>12);
-console.log(4>3||10>12);
-console.log(!(4>3),'v');
-console.log(!(4<3));
-console.log(!(false));
-console.log(!(4>3 && 10<12));
-console.log(!(4 > 3 && 10 > 12));
-console.log(!(4 === '4'));
-console.log('dragon'.includes('on')!=='python'.includes('on'));
+// //6 Figure out the result of the following expressions first without using
+// //  console.log(). After you decide the result confirm it by using console.log()
+// console.log(4>3&&10<12,'i');
+// console.log(4>3&&10>12);
+// console.log(4<3 || 10>12);
+// console.log(4>3||10>12);
+// console.log(!(4>3),'v');
+// console.log(!(4<3));
+// console.log(!(false));
+// console.log(!(4>3 && 10<12));
+// console.log(!(4 > 3 && 10 > 12));
+// console.log(!(4 === '4'));
+// console.log('dragon'.includes('on')!=='python'.includes('on'));
 
-//7 Use the Date object to do the following activities
+// //7 Use the Date object to do the following activities
 
-let naw = new Date()
-console.log(naw.getFullYear());
-console.log(naw.getMonth());
-console.log(naw.getDate())
-console.log(naw.getUTCDay())
-console.log(naw.getHours());
-console.log(naw.getMinutes());
-console.log(naw.getSeconds());
-const allSeconds=Date.now()
-console.log(allSeconds);
+// // let naw = new Date()
+// // console.log(naw.getFullYear());
+// // console.log(naw.getMonth());
+// // console.log(naw.getDate())
+// // console.log(naw.getUTCDay())
+// // console.log(naw.getHours());
+// // console.log(naw.getMinutes());
+// // console.log(naw.getSeconds());
+// // const allSeconds=Date.now()
+// // console.log(allSeconds);
+// // console.log(naw.getTime(milliseconds, since ,January, 1,1980));
+
+// // Exercises: Level 2
+// 1
+// // let base= prompt('Enter the base of a triangle: ');
+// // let height=prompt('Enter the height of a triangle: ');
+// // let all= base*height
+// // console.log(`The area of triangle is ${all}`);
+
+// // 2avaScript program to find the area of a triangle
+// // let A = parseInt( prompt('Enter side a: '))
+// // let B =parseInt( prompt( 'Enter side b: '))
+// // let C =parseInt( prompt('Enter side c: '))
+// // let abc= (A+B+C)
+// // console.log('The perimeter of the triangle is:',abc);
+
+
+// 3
+// // var length = parseInt(prompt("Enter a whole number for the length of your rectangle."));
+// // var width = parseInt(prompt("Enter a whole number for the width of your rectangle."));
+// // function area(length, width) {
+// //     return length * width;
+// // }
+// // function perimeter(length, width) {
+// //     return 2*( length + width);
+// // }
+
+// // console.log('The area of your rectangle is ' + area(length, width));         
+// // console.log('The perimeter of your rectangle is ' + perimeter(length, width));
+
+// 4
+// // let pi = parseInt(prompt('Enter a whole number for the pi of your circle: '))
+// // let pi = 3.14
+// // let r = parseInt(prompt('Enter a whole number for the radius of your circle: '))
+// // function are(pi,r){
+// //   return pi*r**3
+// // }
+// //  function per(pi,r){
+// //    return 2 *pi *r
+// //  }
+// // console.log('calculate the area of a circle ',are(pi,r));
+// // console.log('circumference of a circle',per(pi,r));
+//  5
+// // let a = 1
+// // let b = 2
+// // let c=Math.round( Math.sqrt((a**2+b**2)));
+// // console.log('lenght',c);
+
+9 
+let hours = parseInt(prompt('Enter hours: '));
+let rate =parseInt(prompt('Enter rate per hour: '))
+let earning = hours*rate;
+console.log(`Your weekly earning is:${earning}`);
+
+// function test(name ,age, email ){
+//   name.length<=4 ? console.log('error'):console.log('kelingiz')
+// age <=15 ? console.log('yoqol'):console.log('welcom')
+// email.length<=10 ? console.log('noto`g`ri'): console.log('to`g`ri')
+// } 
+// test('AZree',17,'abdumu@gmail')
+ //10 
+// // let name = prompt('write your name: ')
+// // // if(name.length<=7){
+// // console.log('short');
+// // }else{
+// //   console.log('long');
+/// // 
+//11
+//  let first = prompt('write your first  Name: ')
+// let last = prompt('write your family  Name: ')
+// if(first.length<=4){
+//   console.log(`Your first name ${first} is short then your family name ${last}`);
+// }else{
+// console.log(`Your family name ${last} is long then your first name ${first}`);
+// }
+//12
+// let myAge = prompt('write your age: ')
+// let yourAge= prompt('wrie another age: ')
+// if(myAge>=100){
+//   console.log(`I am ${myAge} years older then you `);
+// }else{
+//   console.log(`You are ${yourAge} years older then me`);
+// }
+
+// var birth = prompt("Please input your Birth year")
+// var now = new Date()
+// var age = now.getYear() - birth + 1900
+
+// if(age<18)
+//     console.log('minor')
+// else if(age>=18 && age<36)
+//     console.log('youth')
+// else console.log('elder')
+
+//13
+
+// let yosh= prompt('enter your birth: ')
+// let yosh2=2021-yosh
+// if(yosh2>=18){
+//   document.writeln(`siz ${2021-yosh} ruxsat bpr`)
+// }else{
+//   document.writeln(`siz ${yosh2} yoshdasiz ruxsat olishingizga xali ${18-yosh2}`)
+// }
+14
+// let now = new Date;
+// let age  = now.getFullYear()-1998
+//  console.log(age);
+//  var sec = 23 * 365 * 24 * 60 * 60;
+//  console.log(`you lived ${sec} secunds`);
+
+
+// var date = new Date('2010-08-10 00:00:00');
+// date = date.toLocaleDateString(undefined, {day:'2-digit'}) + '-' + date.toLocaleDateStr
+
+// function init(){
+//   var d = new Date();
+//   var day = d.getDate();
+//   var x = d.toDateString().substr(4, 3);
+//   var year = d.getFullYear();
+//   document.querySelector("#mydate").innerHTML = day + '-' + x + '-' + year;
+// }
+// window.onload = init;
+
+15
+//  const now = new Date()
+// const year = now.getFullYear() // return year
+// const month = now.getMonth() + 1 // return month(0 - 11)
+// const date = now.getDate() // return date (1 - 31)
+// const hours = now.getHours() // return number (0 - 23)
+// const minutes = now.getMinutes() // return number (0 -59)
+
+// console.log(`${year}-${month}-${date} ${hours}:${minutes}`) // 4/1/2020 0:56
+// console.log(`${date}-${month}-${year} ${hours}:${minutes}`) // 4/1/2020 0:56
+// console.log(`${date}/${month}/${year} ${hours}:${minutes}`) // 4/1/2020 0:56
+
+//Exercises: Level 3
+
+let now  =  new Date;
+let year = now.getFullYear()
+const month = now.getMonth() + 1 // return month(0 - 11)
+const date = now.getDate() // return date (1 - 31)
+ const hours = now.getHours() // return number (0 - 23)
+ const minutes = now.getMinutes() // return number (0 -59)
+
+ console.log(year,`0${month}:${date}:${hours}:${minutes}`);
--- #Day 04 — Conditionals/script.js
+++ #Day 04 — Conditionals/script.js
@@ -1,5 +1,7 @@
-1
 //let you = prompt('Enter your age:');
+
+let you = prompt("Enter your age:");
+
 // if(you>=18){
 //     console.log('You are old enough to drive.');
 // }else if(you !== 'number'){
@@ -9,20 +11,188 @@
 //     console.log(`You are left with ${18-you} years to drive.`);
 // }
 // you>=18 ? console.log('You are old enough to drive.'): console.log(`You are left with ${18-you} years to drive.`);
+
 // 2
-let me =prompt('Enter your age:')
-let yosh=me -2021
-if(yosh>=18){
-    console.log(`I am ${yosh} older then you `)
-}else{
-    console.log(`You are ${yosh} older then me`);
-}
+// const me =prompt('Enter yor age:')
+// const you = prompt('Enteeeeeer your age:')
+
+//  const tosh= 2021-you
+//  const yosh = 2021 -me
+// if(me<you){
+//     console.log(`I am ${yosh} older then you ${tosh}`)
+// }else{
+//     console.log(`You are ${tosh} older then me ${yosh}`);
+// }
 //function abbos(son) {
 
 //     let string=" "
 //          for(i=1;i<=10;i++){
 //     string=`${son}*${i}=${son*i}`
-//     console.log(  string);       
+//     console.log(  string);
 //          }
 //      }
 //      abbos(40)
+
+// let a = [1,10,55,66,68,85,256,254]
+// let low = 0
+// let high = a.length-1
+// let mid = (low + high)
+// if(mid%2!==0) {
+//     console.log(a[parseInt(mid/2)]);
+// }
+// else if(mid%2==0) {
+//     console.log(a[mid/2]);
+// }
+
+// let a = prompt('Enter number')
+// let b = prompt('Enter number')
+
+// a>b ? console.log(`${a} is greater then ${b}`):console.log(`${b} is greater then ${a}`);
+
+// let a = prompt('Enter a number:' )
+// if(a%2==0){
+//     document.writeln(`${a} is an even number`)
+// }else{
+//     document.writeln(`${a} is an odd number`)
+// }
+//Exercises: Level 2
+
+// let a = prompt('Balll')
+// if(a>=80){
+//     console.log(`${a},A`);
+// }
+// else if(a>=70){
+// console.log(`${a},B`);
+// }
+// else if(a>=60){
+// console.log(`${a},C`);
+
+// }
+// else if(a>=50){
+//     console.log(`${a},D`);
+// }
+// else if(a>=0){
+//     console.log(`${a},F`);
+// }else{
+//     console.log('Idi jopu');
+// }
+
+// let a =prompt('Enter the season ')
+// switch(a){
+//     case '12':
+//     case '1':
+//     case '2':
+//         console.log('winter');
+//         break;
+
+//         case '3':
+//         case '4':
+//         case '5':
+//             console.log('spring');
+//             break;
+
+//             case '6':
+//             case '7':
+//             case '8':
+//                 console.log('summer');
+//                 break;
+//                 case '9':
+//                 case '10':
+//                 case '11':
+//                     console.log('fall');
+//                     break;
+//                     default:
+//                         console.log("Sorry, we are not find of Season");
+// }
+
+// function getSeason() {
+//     month = document.forms.date.month.value;
+//     season = '';
+//     switch(month) {
+//         case '12':
+//         case '1':
+//         case '2':
+//             season = 'winter';
+//         break;
+//         case '3':
+//         case '4':
+//         case '5':
+//             season = 'spring';
+//         break;
+//         case '6':
+//         case '7':
+//         case '8':
+//             season = 'summer';
+//         break;
+//         case '9':
+//         case '10':
+//         case '11':
+//             season = 'fall';
+//         break;
+//     }
+//     alert(season);
+// }
+
+// var d = new Date();
+// var n = d.getDay();
+//  if( n == 4 )
+// console.log("Its weekend!!");
+// else
+// console.log("Its not weekend");
+// const day = yourDateObject.isoWeekday();
+// // or const day = yourDateObject.get('isoWeekday');
+// const isWeekend = (day === 6 || day === 7);
+
+// var dayOfWeek = yourDateObject.getDay();
+// var isWeekend = (dayOfWeek === 6) || (dayOfWeek  === 0);
+
+let a = prompt("What is the day today ?").toLowerCase();
+//  let b = a.toLowerCase().prompt('heheheh')
+switch (a) {
+  case "monday":
+  case "Tuesday":
+  case "Wednezday":
+  case "Thursday":
+  case "Friday":
+    document.writeln("Working day");
+    break;
+  case "Saturday":
+  case "Sunday":
+    document.writeln("Weekend");
+    break;
+  default:
+    document.writeln("Что за хрен");
+}
+// Exercises: Level 3
+let month = prompt("Enter month").toLowerCase();
+switch (month) {
+  case "december":
+    document.writeln("December ");
+  case "1":
+  case "2":
+    season = "winter";
+    break;
+  case "3":
+  case "4":
+  case "5":
+    season = "spring";
+    break;
+  case "6":
+  case "7":
+  case "8":
+    season = "summer";
+    break;
+  case "9":
+  case "10":
+  case "11":
+    season = "fall";
+    break;
+}
+
+let me = prompt("Enter your age:");
+let yosh = me - 2021;
+if (yosh >= 18) {
+  console.log(`I am ${yosh} older then you `);
+} else {
+  console.log(`You are ${yosh} older then me`);
+}
