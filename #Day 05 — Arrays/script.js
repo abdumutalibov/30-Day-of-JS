7b937ffcfdba81a7a04019ac06f5f5d0514ece53
Abduali <dreyfus1309@gmail.com>
2021-10-14 23:57:26 +0500

    Exercise :Level 2

--- #Day 05 — Arrays/script.js
+++ #Day 05 — Arrays/script.js
@@ -1,11 +1,29 @@
-let arr = ['python','Node','Redux','MongoDB','JavaScript']
-console.log(arr.length);
-console.log(arr[0],arr[3],arr[4]);
-const mixedDataTypes = ['called','Declare','python','Node','Redux','MongoDB','JavaScript']
-console.log(mixedDataTypes.length);
+// let arr = ['python','Node','Redux','MongoDB','JavaScript']
+// console.log(arr.length);
+// console.log(arr[0],arr[3],arr[4]);
+// const mixedDataTypes = ['called','Declare','python','Node','Redux','MongoDB','JavaScript']
+// console.log(mixedDataTypes.length);
 let itCompanies =['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle' , 'Amazon']
-console.log(itCompanies);
-console.log(itCompanies.length);
-console.log(itCompanies[0],itCompanies[3],itCompanies[6]);
-console.log(itCompanies);
-console.log(itCompanies.t);
+// console.log(itCompanies);
+// console.log(itCompanies.length);
+// console.log(itCompanies[0],itCompanies[3],itCompanies[6]);
+// console.log(itCompanies);
+// // console.log(itCompanies.join());
+// let oo = []
+// for(let i =0; i<itCompanies.length;i++){
+//     if(itCompanies[i].includes("oo")){
+//         oo.push(itCompanies[i])
+//     }
+// }
+// console.log(oo+'h');
+
+// console.log(itCompanies.sort().reverse());
+// console.log(itCompanies.slice(3));
+// console.log(itCompanies.splice(0,4));
+// console.log(itCompanies.slice(3,4));
+// console.log(itCompanies.shift()+6);
+console.log(itCompanies.pop(3));
+console.log(itCompanies.splice());
+
+
+//Exercise: Level 2
