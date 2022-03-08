73f81127f1dd55a4fc9e62fb8ed69ab28f12710d
abdumutalibov <azimabdumutalibov21@gmail.com>
2022-03-09 01:15:41 +0500

    # day-9

--- #Day 09 — Higher Order Functions/script.js
+++ #Day 09 — Higher Order Functions/script.js
@@ -52,41 +52,147 @@
 // }
 // setInterval(sayHello(), 2000)
 
-let sum =0
-const num =[1,2,3,4]
-num.forEach(num => sum+=num)
-    console.log(sum);
-let g =0
-let t = [1,2,3,5,6]
-t.forEach(y=> console.log(y))
+// let sum =0
+// const num =[1,2,3,4]
+// num.forEach(num => sum+=num)
+//     console.log(sum);
+// let g =0
+// let t = [1,2,3,5,6]
+// t.forEach(y=> console.log(y))
 
-const country = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
-country.forEach((e)=>console.log(e.toLocaleLowerCase()))
+// const country = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+// country.forEach((e)=>console.log(e.toLocaleLowerCase().slice(0,3)))
 
-// map
+// // map
 
-const number =[1,2,3,4,5]
-const numbers=number.map((n)=>n*n)
-console.log(numbers);
+// const number =[1,2,3,4,5]
+// const numbers=number.map((n)=>n*n)
+// console.log(numbers);
 
+// const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+// const namesE=names.map((e)=>e.toLocaleUpperCase())
+// console.log(namesE);
+
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya',
+//   ]
+
+//   const c=countries.map((e)=> e.toUpperCase().slice(0,3))
+//   console.log(c);
+  
+//   const tr =countries.filter((u)=> u.includes('land'))
+//   console.log(tr);
+
+//   const r = countries.filter((c)=>c.endsWith('ia'))
+//   console.log(r);
+
+// //   const yu = countries.map((e)=>{
+// //       return console.log(e.includes('Can'));
+// //   })
+//   const countriesToUpperCase = countries.filter(country => country.length === 5);
+//   console.log(countriesToUpperCase);
+
+//   const scores = [
+//     { name: 'Asabeneh', score: 95 },
+//     { name: 'Mathias', score: 80 },
+//     { name: 'Elias', score: 50 },
+//     { name: 'Martha', score: 85 },
+//     { name: 'John', score: 100 },
+//   ]
+  
+//   const m = scores.filter((s)=>{
+//      return s.score >80})
+//   console.log(m);
+
+//   const j = [1,2,3,4,5]
+//   const k =scores.reduce((t,acc)=>{
+//   if(acc.score <=80){
+//       t+=acc.score
+//   }
+//     return t
+//   },0)
+//   console.log(k);
+  
+
+//   const naames = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+
+//   const all =naames.every((name)=> {
+//    return typeof name === 'string'})
+//   console.log(all);
+
+//   function getComp(str){
+//     return str
+//     .toLowerCase()
+//     .match(/(.)\1+/gi)
+//     .reduce((acc, item)=> acc+ item[0] + item.length, '')
+
+//   }
+//   console.log(getComp('aaaAAaaBbbBBbFffFFffF'));
+
+//   //find
+
+//   const ages =  [24, 22, 25, 32, 35, 18]
+//   const age = ages.find((e)=> e<20)
+//   console.log(age);
+
+
+//   const namest = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+//   const o = namest.find((e)=> e.length >7)
+//   console.log(o);
+
+//   const p=scores.find((e)=>e.score<80)
+//   console.log(p);
+
+
+//   const namess = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+// const agess = [24, 22, 25,21, 32, 35, 18]
+
+// const l=namess.findIndex((e)=>e=='Brook')
+// console.log(l);
+// const b=agess.findIndex((e)=>e<33)
+// console.log(b);
+
+
+// const nam = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+// const bools = [true, true, true, true]
+
+// const d=nam.some((b)=>{
+//   return b==='Elias'
+// })
+// console.log(d);
+
+// const ns = [9.81, 3.14, 100, 37]
+// ns.sort((a,v)=> a-v)
+// console.log(ns);
+
+// scores.sort((a,b)=>{
+//   if(a.score < b.score) return -1
+//   if(a.score > b.score) return 1
+//   return 0
+// })
+// console.log(scores);
+
+//Exercises: Level 1
+
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
-const namesE=names.map((e)=>e.toLocaleUpperCase())
-console.log(namesE);
-
-
-const countries = [
-    'Albania',
-    'Bolivia',
-    'Canada',
-    'Denmark',
-    'Ethiopia',
-    'Finland',
-    'Germany',
-    'Hungary',
-    'Ireland',
-    'Japan',
-    'Kenya',
-  ]
-
-  const c=countries.map((e)=> e.toLocaleLowerCase())
-  console.log(c);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
