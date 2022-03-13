cc15b4a80c6eb95fb870eb3e7e97ba21ec3914a2
abdumutalibov <azimabdumutalibov21@gmail.com>
2022-03-14 01:44:16 +0500

    # Day 9

--- #Day 09 — Higher Order Functions/main.js
+++ #Day 09 — Higher Order Functions/main.js
@@ -76,3 +76,68 @@ console.log(StartsWith(countries));
 
 
 // Exercises: Level 3
+
+const world=[
+    {country: 'English',count:91},
+    {country: 'French',count:45},
+    {country: 'Arabic',count:25},
+    {country: 'Spanish',count:24},
+    {country:'Russian',count:9},
+    {country:'Portuguese', count:9},
+    {country:'Dutch',count:8},
+    {country:'German',count:7},
+    {country:'Chinese',count:5},
+    {country:'Swahili',count:4}
+    ]
+    const hr =world.sort((a,b)=>{
+        if(a.count <b.count) return -1
+        if(a.count >b.count) return 1
+    })
+    console.log(hr);
+
+
+    
+    
+      const mostSpokenLanguages =(num)=>{
+          let empty =[]
+ world.forEach((element,index)=>{
+    if(index < num && element.count<100){
+       empty.push(element)
+    }
+})
+return empty
+}
+      console.log(mostSpokenLanguages(3));
+
+  const data  =  [
+        {country: 'China', population: 1377422166},
+        {country: 'India', population: 1295210000},
+        {country: 'United States of America', population: 323947000},
+        {country: 'Indonesia', population: 258705000},
+        {country: 'Brazil', population: 206135893},
+        {country: 'Pakistan', population: 194125062},
+        {country: 'Nigeria', population: 186988000},
+        {country: 'Bangladesh', population: 161006790},
+        {country: 'Russian Federation', population: 146599183},
+        {country: 'Japan', population: 126960000}
+        ]
+const mostPopulatedCountries =(sum)=>{
+    data.forEach((element,index)=>{
+
+    })
+}
+        console.log(mostPopulatedCountries(countries, 10))
+
+        const  getBla=(arr)=> {
+            let m =[]
+             data.forEach((value,index)=>{
+                 if(index < arr ){
+                    //  m.push(value.population); 
+                     m.push(value)
+                 }
+                 
+                })
+                return  m
+        //    return (Math.max(...m)); 
+          }
+          console.log( getBla(3))
