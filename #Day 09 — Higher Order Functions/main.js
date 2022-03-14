d7de6d1306843826067ef43319beb7f20f203943
abdumutalibov <azimabdumutalibov21@gmail.com>
2022-03-15 00:12:58 +0500

    # Day 9

--- #Day 09 — Higher Order Functions/main.js
+++ #Day 09 — Higher Order Functions/main.js
@@ -141,3 +141,5 @@ const mostPopulatedCountries =(sum)=>{
         //    return (Math.max(...m)); 
           }
           console.log( getBla(3))
+
+          const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
