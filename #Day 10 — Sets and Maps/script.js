b0157bb29000b8a6ff34952b2f49ecd60a89841f
abdumutalibov <azimabdumutalibov21@gmail.com>
2022-03-17 01:44:58 +0500

    a

--- #Day 10 — Sets and Maps/script.js
+++ #Day 10 — Sets and Maps/script.js
@@ -91,4 +91,55 @@ let cc = aa.filter((num)=> !BB.has(num))
 let CC = new Set(cc)
 
 console.log(CC);
-console.log(CC);
+
+//Map
+
+// const map = new Map()
+// console.log(map);
+
+countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo'],
+  ]
+  const map = new Map(countries)
+  console.log(map);
+  console.log(map.size);
+  //Adding values to the Map
+  const countriesMap = new Map()
+  console.log(countriesMap.size);
+  countriesMap.set('Finland', 'Helsinki')
+  countriesMap.set('Sweden', 'Stockholm')
+  countriesMap.set('Norway', 'Oslo')
+  console.log(countriesMap);
+
+//Getting a value from Map
+
+console.log(countriesMap.get('Finland'));
+
+
+//Checking key in Map
+
+console.log(countriesMap.has('Finland'));
+
+//Getting all values from map using loop
+
+for(const country of countriesMap){
+    console.log(country);
+}
+for(const [country, city] of countriesMap){
+    console.log(country, city);
+}
+//Exercises:Level 1
+
+const companies = new Set()
+console.log(companies);
+
+for(let i=0; i<=10;i++){
+    companies.add(i);
+}
+console.log(companies);
+companies.delete(2)
+console.log(companies);
+companies.clear()
+console.log(companies);
